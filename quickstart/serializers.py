ee7563293aa74c74fc1eb57b730322396a6427d3
Prabhat <prabhat.namdharani@learner.manipal.edu>
2022-06-23 00:27:13 +0530

    Working with views

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -2,6 +2,13 @@ from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES  
 
+class UserSerializer(serializers.ModelSerializer):
+    snippets=serializers.PrimaryKeyRelatedField(many=True,queryset=Snippet.objects.all())
+
+    class Meta:
+        model=User
+        fields=['id','username','snippets']
+        
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model=Snippet
