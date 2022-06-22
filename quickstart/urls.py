0380d481e1d8a24e68e532fa92b6501411dbc6bb
Prabhat <prabhat.namdharani@learner.manipal.edu>
2022-06-22 14:23:50 +0530

    Class based views

--- quickstart/urls.py
+++ quickstart/urls.py
@@ -2,9 +2,17 @@ from django.urls import path, include
 from . import views 
 from rest_framework.urlpatterns import format_suffix_patterns
 
+# Function based views
+# urlpatterns=[
+#     path('snippets/',views.snippet_list), 
+#     path('snippets/<int:pk>/',views.snippet_detail),
+# ]
+
+# Class based views
+
 urlpatterns=[
-    path('snippets/',views.snippet_list), 
-    path('snippets/<int:pk>/',views.snippet_detail),
+    path('snippets/',views.SnippetList.as_view()), 
+    path('snippets/<int:pk>/',views.SnippetDetail.as_view()),
 ]
 
 urlpatterns=format_suffix_patterns(urlpatterns)
